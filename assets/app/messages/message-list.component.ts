2ab4871d0b6cf7c465444e3fdbf68e07de825ac0
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-04-28 16:26:50 +0000

    Added hierarchical service providers

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component ,OnInit } from "@angular/core";
 
 import { Message} from "./message.model";
+import { MessageService } from "./message.service";
 
 @Component({
     selector: 'app-message-list',
@@ -15,10 +16,12 @@ import { Message} from "./message.model";
     `
 })
 
-export class MessageListComponent {
-    messages: Message[] = [
-        new Message('Some message', 'Aritra'),
-        new Message('Something else', 'Aritra'),
-        new Message('Something useless', 'Aritra')
-    ]
+export class MessageListComponent implements OnInit {
+    messages: Message[];
+    
+    constructor(private messageService: MessageService){}
+    
+    ngOnInit(){
+        this.messages = this.messageService.getMessages();
+    }
 }
