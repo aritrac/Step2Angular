974c66d6c376ff81a05113b2b5f009d6179a4749
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-04-28 16:46:58 +0000

    Added angular form validation

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 import { MessageService } from "./message.service";
-import { Message } from "./message.model"; 
+import { Message } from "./message.model";
 
 @Component({
     selector: "app-message-input",
@@ -11,8 +12,9 @@ import { Message } from "./message.model";
 export class MessageInputComponent{
     constructor(private messageService: MessageService){}
     
-    onSave(value: string){
-        const message = new Message(value,'Aritra');
+    onSubmit(form: NgForm){
+        const message = new Message(form.value.content,'Aritra');
         this.messageService.addMessage(message);
+        form.resetForm();
     }
 }
