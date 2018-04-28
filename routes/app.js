628104dcbc2420c73afc1f493ccf7d2af4dac702
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-04-28 13:48:12 +0000

    Added 2 way data binding

--- routes/app.js
+++ routes/app.js
@@ -1,31 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc){
-        if(err){
-            return res.send('Error!');
-        }
-        res.render('node', {email: doc.email});
-    });
-});
-
-router.post('/',function(req,res,next){
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Aritra',
-        lastName: 'Chatterjee',
-        password: 'super-secret',
-        email: email
-    });
-    user.save(function(err,savedUser){
-        if(err){
-            return console.error(err);
-        }
-        console.log(savedUser);
-    });
-    res.redirect('/');
+    res.render('index');
 });
 
 module.exports = router;
