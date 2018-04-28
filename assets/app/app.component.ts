a9b09744493eaea8682df82a85dd8bbc9034286a
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-04-28 15:58:12 +0000

    Added html element reference

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,29 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Message } from "./messages/message.model";
-
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styles: [`
-        .author {
-            display: inline-block;
-            font-style: italic;
-            font-size: 12px;
-            width: 80%
-        }
-        .config {
-            display: inline-block;
-            text-align: right;
-            font-size: 12px;
-            widht: 19%
-        }
-    `]
 })
 export class AppComponent {
-    messages: Message[] = [
-        new Message('Some message', 'Aritra'),
-        new Message('Something else', 'Aritra'),
-        new Message('Something useless', 'Aritra')
-    ]
+    
 }
