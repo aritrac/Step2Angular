be1b934675578b292b11cc80e7431d09695ded6d
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-04-29 05:45:35 +0000

    Added message delete functionality

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -8,5 +8,18 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 })
 
 export class SigninComponent implements OnInit {
+    myForm: FormGroup;
     
+    onSubmit(){
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
+    
+    ngOnInit(){
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        this.myForm = new FormGroup({
+            email: new FormControl(null, [Validators.required, Validators.pattern(re)]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
