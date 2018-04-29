11c0b2a244bfa15b55e3722a889eb487ba1864bd
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-04-29 04:51:03 +0000

    finished signup form

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-signin',
     templateUrl: './signin.component.html'
 })
 
-export class SigninComponent {
+export class SigninComponent implements OnInit {
     
 }
