9296567374599e7631c8597e5a8f34dec411c8af
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-05-05 15:44:18 +0000

    Added table relationship between user and messages

--- models/message.js
+++ models/message.js
@@ -1,9 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var  User = require('./user');
 
 var schema = new Schema({
     content: {type: String, required: true},
     user: {type: Schema.Types.ObjectId}
+});
+
+schema.post('remove', function(message){
+    User.findById(message.user, function(err, user) {
+        if(err){
+            //TODO
+        }
+        user.messages.pull(message);
+        user.save();
+    });
 });
 
 module.exports = mongoose.model('Message',schema);
