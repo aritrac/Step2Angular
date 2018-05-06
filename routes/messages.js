8fba9c6bc215e99a9326463aec651363bfc842aa
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-05-06 02:23:50 +0000

    Added edit and delete route protection bases on user

--- routes/messages.js
+++ routes/messages.js
@@ -64,6 +64,7 @@ router.post('/', function (req, res, next) {
 });
 
 router.patch('/:id', function(req,res,next){
+    var decoded = jwt.decode(req.query.token);
     Message.findById(req.params.id,function(err,message){
         if(err){
             return res.status(500).json({
@@ -77,6 +78,12 @@ router.patch('/:id', function(req,res,next){
                 error: {
                     message: 'Message not found'
                 }
+            });
+        }
+        if (message.user != decoded.user._id) {
+            return res.status(401).json({
+                title: 'Not Authenticated',
+                error: {message: 'Users do not match'}
             });
         }
         message.content = req.body.content;
@@ -96,6 +103,7 @@ router.patch('/:id', function(req,res,next){
 });
 
 router.delete('/:id', function(req,res,next){
+    var decoded = jwt.decode(req.query.token);
     Message.findById(req.params.id,function(err,message){
         if(err){
             return res.status(500).json({
@@ -109,6 +117,12 @@ router.delete('/:id', function(req,res,next){
                 error: {
                     message: 'Message not found'
                 }
+            });
+        }
+        if (message.user != decoded.user._id) {
+            return res.status(401).json({
+                title: 'Not Authenticated',
+                error: {message: 'Users do not match'}
             });
         }
         message.remove(function(err, result){
