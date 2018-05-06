f9dbfedce25b1090bccd6247ac5676a8878a8098
aritrachatterjee2007@gmail.com <aritrachatterjee2007@gmail.com>
2018-05-06 03:39:28 +0000

    Added error service

--- assets/app/errors/error.component.ts
+++ assets/app/errors/error.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { Error } from "./error.model";
 
@@ -16,11 +16,23 @@ import { Error } from "./error.model";
         }
     `]
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit {
     error: Error;
     display = 'none';
     
+    constructor(private errorService: ErrorService) {}
+    
     onErrorHandled(){
         this.display = 'none';
+    }
+    
+    ngOnInit() {
+        this.errorService.errorOccurred.
+            subscribe(
+                (error: Error) => {
+                    this.error = error;
+                    this.display = 'block';
+                }
+            );
     }
 }
